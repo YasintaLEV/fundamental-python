a49b80b7cc49d2f52fc53df4b3e1b84e45be929b
Yasinta LEV <yasinta.ladyeka@gmail.com>
2021-02-27 00:46:11 +0700

    Jalan lurus aja!

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import requests
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+print('Hello World!')
+print('by Yasinta')
+print('tanggal 26 februari 2021')
+print('-' * 10)
 
-print("Hello World!")
-try:
-    r = requests.get("https://google.com")
-    if r.status_code == 200:
-        print(r.text)
-except Exception as e:
-    print('404 eror', e)
+# PERCABANGAN: Eksekusi terpilih
+ingin_cepat = True
+if ingin_cepat:
+    print('Jalan lurus aja ya!')
+else:
+    print('Jalan lain')
+
+# PENCABANGAN PILIHAN LEBIH DARI 2
+ingin_balik = True
+
+# PERULANGAN
+jumblah_anak = 4
+
+for index_anak in range(1, 5):  # jumlah perulangan = 5 - 1 = 4
+    print (f'Halo anak {index_anak}')
