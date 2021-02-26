ce16beece32866227d25f0bf4a0711608fdccbe7
Yasinta LEV <yasinta.ladyeka@gmail.com>
2021-02-26 18:36:57 +0700

    apa saja

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 
 print("Hello World!")
 try:
-    r = requests.get("https://go ogle.com")
+    r = requests.get("https://google.com")
     if r.status_code == 200:
         print(r.text)
 except Exception as e:
-    print('ada error', e)
+    print('404 eror', e)
