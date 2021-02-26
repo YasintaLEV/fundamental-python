7f9c676a3bf264bd9002672e35363331ceb44f48
Yasinta LEV <yasinta.ladyeka@gmail.com>
2021-02-26 18:43:49 +0700

    ayo aja ...

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 print("Hello World!")
 try:
-    r = requests.get("https://aroepz.com")
+    r = requests.get("https://google.com")
     if r.status_code == 200:
         print(r.text)
 except Exception as e:
