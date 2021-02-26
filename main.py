995c2ce0bcef20d184eefcfc2e921ab426ef4962
YasintaLEV <yasinta.ladyeka@gmail.com>
2021-02-26 16:58:11 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+print("Hello World!")
+try:
+    r = requests.get("https://go ogle.com")
+    if r.status_code == 200:
+        print(r.text)
+except Exception as e:
+    print('404 not found', e)
